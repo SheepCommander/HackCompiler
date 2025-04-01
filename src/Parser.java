2d9f1d398d2520cdcfb493780d3c7354b190b890
SheepCommander <66646514+SheepCommander@users.noreply.github.com>
2025-04-01 03:49:25 -0700

    test

---
+++ src/Parser.java
@@ -0,0 +1,9 @@
+/**
+ * Parser object: Stores data of
+ */
+public class Parser {
+    // fields: variable hashmap, label hashmap
+    public Parser() {
+        // Parser will be an object whose behavior is used by the Compiler
+    }
+}
